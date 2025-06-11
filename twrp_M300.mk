d76426cbe1c8a9e1b178cce8a3ad7a6715ec1460
bdOtopsy63 <108967802+bdOtopsy63@users.noreply.github.com>
2025-06-11 12:29:31 +0200

    Update and rename omni_M300.mk to twrp_M300.mk

--- twrp_M300.mk
+++ twrp_M300.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from M300 device
 $(call inherit-product, device/hiby/M300/device.mk)
 
 PRODUCT_DEVICE := M300
-PRODUCT_NAME := omni_M300
+PRODUCT_NAME := twrp_M300
 PRODUCT_BRAND := HIBYDIGITAL
 PRODUCT_MODEL := M300
 PRODUCT_MANUFACTURER := hiby
