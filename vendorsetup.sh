d86adc4c4cbb103d72e615b27f6414f77c5860a1
bdOtopsy63 <108967802+bdOtopsy63@users.noreply.github.com>
2025-06-11 12:25:29 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_M300-user
-add_lunch_combo omni_M300-userdebug
-add_lunch_combo omni_M300-eng
+add_lunch_combo twrp_M300-user
+add_lunch_combo twrp_M300-userdebug
+add_lunch_combo twrp_M300-eng
